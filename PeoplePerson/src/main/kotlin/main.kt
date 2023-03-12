82a5d7277bbbe653e1c92baf18bfcbefd9e9709d
twin730 <twin730@mail.ru>
2023-03-12 23:44:19 +0300

    main.kt changed

--- PeoplePerson/src/main/kotlin/main.kt
+++ PeoplePerson/src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    var likes = 24
+    var likes = 3
 
     println("Понравилось " + (likes) + " " + (peoplePersonChoice(likes)))
 
@@ -7,12 +7,22 @@ fun main() {
 
 fun peoplePersonChoice(likes: Int): String {
     return when {
-        likes == 1 -> "человеку"
-        likes in (2..4) -> "людям"
+        (likes % 10) == 1 -> "человеку"
+        (likes % 10) != 1 -> "людям"
         likes in (5..20) -> "человекам"
-        likes > 20 && likes % 2 != 0 -> "человекам"
-        likes > 20 && likes % 2 == 0 -> "людям"
         else -> "(в-общем, никому не понравилось)"
     }
-
 }
+
+
+////fun peoplePersonChoice(likes: Int): String {
+////    return when {
+////        likes == 1 -> "человеку"
+////        likes in (2..4) -> "людям"
+////        likes in (5..20) -> "человекам"
+////        likes > 20 && likes % 2 != 0 -> "человекам"
+////        likes > 20 && likes % 2 == 0 -> "людям"
+////        else -> "(в-общем, никому не понравилось)"
+////    }
+//
+//}
